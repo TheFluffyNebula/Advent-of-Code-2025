36e133883a5b04375907f691561308761aeed8f2
Jason Yin <jyin23@ncsu.edu>
2025-12-04 15:56:18 -0500

    Day 4 😴

---
+++ 04/Part 2/j.py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readlines
+
+board = input()
+n = len(board)
+for i in range(n):
+    board[i] = list(board[i].strip())
+# print(board)
+
+DIRECTIONS = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+
+ans = 0
+
+changes = 1
+while changes:
+    changes = 0
+    for i in range(n):
+        for j in range(len(board[0])):
+            cur = 0
+            for dx, dy in DIRECTIONS:
+                nx, ny = i + dx, j + dy
+                if nx < 0 or nx >= n or ny < 0 or ny >= len(board[0]):
+                    continue
+                # print(nx, ny)
+                if board[nx][ny] == "@":
+                    cur += 1
+            if cur < 4 and board[i][j] == "@":
+                ans += 1
+                changes += 1
+                board[i][j] = "."
+
+print(ans)
